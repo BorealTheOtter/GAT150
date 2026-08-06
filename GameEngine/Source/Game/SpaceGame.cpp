e785e630744f621b3e2410a90718652e2687b703
BorealTheOtter <thethomasuser93@gmail.com>
2026-08-06 10:32:54 -0600

    Load fonts by ID; remove menuFont.ttf
    Delete the unused GameEngine/Build/Assets/Fonts/menuFont.ttf asset and update SpaceGame.cpp to use sr::Resources::GetWithID for fonts. The menu and game fonts are now loaded by ID ("MenuFont", "GameFont") and both reference Assets/Fonts/gameFont.ttf with sizes 64 and 32 respectively, centralizing font usage.

--- GameEngine/Source/Game/SpaceGame.cpp
+++ GameEngine/Source/Game/SpaceGame.cpp
@@ -21,8 +21,8 @@ bool SpaceGame::Initialize()
 	sr::Engine::Get().GetAudio().AddSound("explosion", "Assets/Sounds/snd_badexplosion.wav");
 	sr::Engine::Get().GetAudio().AddSound("shoot", "Assets/Sounds/snd_bomb.wav");
 
-	m_menuFont = sr::Resources().Get<sr::Font>("Assets/Fonts/menuFont.ttf", 64);
-	m_gameFont = sr::Resources().Get<sr::Font>("Assets/Fonts/gameFont.ttf", 32);
+	m_menuFont = sr::Resources().GetWithID<sr::Font>("MenuFont","Assets/Fonts/gameFont.ttf", 64);
+	m_gameFont = sr::Resources().GetWithID<sr::Font>("GameFont","Assets/Fonts/gameFont.ttf", 32);
 
 	m_titleText = new sr::Text(m_menuFont);
 	m_titleText->Create(sr::Engine::Get().GetRenderer(), "Space Shooter", sr::Vector3{ 1.0f, 1.0f, 1.0f });
