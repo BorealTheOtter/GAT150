a1a70c145e231f1c143f7e10bdbf8711c1af5f39
BorealTheOtter <thethomasuser93@gmail.com>
2026-08-11 09:12:30 -0600

    Remove debug JSON loading from Main.cpp
    Remove temporary debug code that loaded and printed Assets/Data/data.json and parsed fields (name, age, speed, isAwake, position, color) using rapidjson/JSON_READ. Keeps Engine initialization intact and cleans up Main.cpp by removing the unused test/debug logic.

--- GameEngine/Source/Game/Main.cpp
+++ GameEngine/Source/Game/Main.cpp
@@ -12,39 +12,6 @@ using namespace sr;
 
 int main()
 {
-    // load the json data from a file
-    std::string buffer;
-    if (ReadTextFile("Assets/Data/data.json", buffer))
-    {
-        // show the contents of the json file (debug)
-        std::cout << buffer << std::endl;
-
-        // create json document from the json file contents
-        rapidjson::Document document;
-        if (json::Load("Assets/Data/data.json", document))
-        {
-            // read/show the data from the json file
-            int age;
-            std::string name;
-            float speed;
-            bool isAwake;
-            Vector2 position;
-            Vector3 color;
-
-            // read the json data
-            JSON_READ(document, name);
-            JSON_READ(document, age);
-            JSON_READ(document, speed);
-            JSON_READ(document, isAwake);
-            JSON_READ(document, position);
-            JSON_READ(document, color);
-
-            // show the data
-            std::cout << name << " " << age << " " << speed << " " << isAwake << std::endl;
-            std::cout << position.x << " " << position.y << std::endl;
-            std::cout << color.r << " " << color.g << " " << color.b << " " << std::endl;
-        }
-    }
 
     //INITIALIZE
     Engine::Get().Initialize();
